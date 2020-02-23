1b81905a0d490f98fddfe72f41b18cf770c88e83
Rusu Maxim <maximrusu18@gmail.com>
2020-02-23 18:50:34 +0200

    Update controller to add DAO into the Model

--- src/com/maxim/spring/controller/CustomerController.java
+++ src/com/maxim/spring/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.maxim.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maxim.spring.dao.CustomerDAO;
+import com.maxim.spring.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// inject DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustumer(Model theModel) {
+		
+		// get the customers from DAO and add them to mvc model
+		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		theModel.addAttribute("custumers",theCustomers);
 		
 		return "customers-list";
 	}
